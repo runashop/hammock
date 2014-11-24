29daa2e106ca97a91978249ec0be4b6df990560e
tankist <stalin1945@gmail.com>
2014-11-24 18:31:32 +0200

    Added ppurl command

--- commands/Ppurl.php
+++ commands/Ppurl.php
@@ -11,16 +11,27 @@ class Ppurl extends AbstractCommand implements CommandInterface
      */
     public function run($command)
     {
-        $redirectUrl = str_rot13(implode('', (array)$command));
-        $query = parse_url($redirectUrl, PHP_URL_QUERY);
+        $command = trim($command);
+        if ($command[0] === '/') {
+            $command = 'http://domain.com' . $command;
+        }
+        $query = parse_url($command, PHP_URL_QUERY);
         if ($query) {
-            parse_str($query, $redirectUrlData);
-            if (isset($redirectUrlData['u'])) {
-                $redirectUrl = $redirectUrlData['u'];
-                $redirectUrlData = json_decode(base64_decode(rawurldecode($redirectUrl)), true);
-                if (isset($redirectUrlData['url'])) {
-                    $redirectUrl = $redirectUrlData['url'];
-                    $this->_message->respond($redirectUrl);
+            parse_str($query, $query);
+            if (isset($query['url'])) {
+                $command = $query['url'];
+                $redirectUrl = str_rot13(implode('', (array)$command));
+                $query = parse_url($redirectUrl, PHP_URL_QUERY);
+                if ($query) {
+                    parse_str($query, $redirectUrlData);
+                    if (isset($redirectUrlData['u'])) {
+                        $redirectUrl = $redirectUrlData['u'];
+                        $redirectUrlData = json_decode(base64_decode(rawurldecode($redirectUrl)), true);
+                        if (isset($redirectUrlData['url'])) {
+                            $redirectUrl = $redirectUrlData['url'];
+                            $this->_message->respond($redirectUrl);
+                        }
+                    }
                 }
             }
         }
