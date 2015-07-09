a30213bd269f87bec0e6b9f1c7ed8143ef40ac73
tankist <stalin1945@gmail.com>
2015-07-09 23:59:37 +0300

    Added PP2 build command

---
+++ commands/Build.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Slack\Command;
+
+/**
+ * Class Build
+ * @package Slack\Command
+ */
+class Build extends AbstractCommand implements CommandInterface
+{
+
+    const PHPCI_URL = 'http://phpci.runashop.net/webhook/bitbucket/1';
+
+    public function run($command)
+    {
+        list($branch, $commit) = array_pad(preg_split('/\s+/', $command), 2, '');
+        if (!$branch) {
+            $branch = 'dev';
+        }
+        $bitbucketPayload = json_encode([
+            'commits' => [
+                [
+                    'raw_author' => 'Victor Gryshko <vgryshko@brightgrove.com>',
+                    'raw_node' => $commit,
+                    'branch' => $branch,
+                    'message' => 'Manual build'
+                ]
+            ],
+        ]);
+
+        try {
+            $request = $this->_client->client->post(self::PHPCI_URL, null, [
+                'payload' => $bitbucketPayload,
+            ]);
+            $response = $request->send();
+
+            $result = json_decode($response->getBody(), true);
+
+            if (!isset($result['status']) || $result['status'] !== 'ok') {
+                throw new \RuntimeException('Build running failed: ' . (isset($result['error']) ? $result['error'] : 'Unknown error'));
+            }
+
+            if (isset($result['commits'])) {
+                foreach ($result['commits'] as $commit => $build) {
+                    if (isset($build['buildId'])) {
+                        $this->_message->respond(sprintf('Build <http://phpci.runashop.net/build/view/%d|#%1$d>', $build['buildId']));
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            $this->_message->respond('Exception: ' . $e->getMessage());
+        }
+
+    }
+
+}
